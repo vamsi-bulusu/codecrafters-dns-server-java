d81af454717a8910980c80cd1a7f77bbdb694796
Vamsi Krishna <bulusu.vk08@gmail.com>
2024-09-23 01:32:45 -0700

    pass 1st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,21 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
-    //   while(true) {
-    //     final byte[] buf = new byte[512];
-    //     final DatagramPacket packet = new DatagramPacket(buf, buf.length);
-    //     serverSocket.receive(packet);
-    //     System.out.println("Received data");
-    //
-    //     final byte[] bufResponse = new byte[512];
-    //     final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
-    //     serverSocket.send(packetResponse);
-    //   }
-    // } catch (IOException e) {
-    //     System.out.println("IOException: " + e.getMessage());
-    // }
+//     Uncomment this block to pass the first stage
+
+     try(DatagramSocket serverSocket = new DatagramSocket(2053)) {
+       while(true) {
+         final byte[] buf = new byte[512];
+         final DatagramPacket packet = new DatagramPacket(buf, buf.length);
+         serverSocket.receive(packet);
+         System.out.println("Received data");
+
+         final byte[] bufResponse = new byte[512];
+         final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
+         serverSocket.send(packetResponse);
+       }
+     } catch (IOException e) {
+         System.out.println("IOException: " + e.getMessage());
+     }
   }
 }
